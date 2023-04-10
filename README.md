845816a3d796acabdf5605760e15b0c278420752
neurolabusc <rorden@sc.edu>
2023-04-10 13:16:45 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# niivue-webgpu
-Using the nascent webgpu API for medical imaging
+## niivue-webgpu
+
+[NiiVue](https://github.com/niivue/niivue) uses WebGL2 to visualize medical imaging data. WebGL2 uses [ANGLE](https://en.wikipedia.org/wiki/ANGLE_(software)) to leverage the optimal graphics language for a device: OpenGL, DirectX, Metal, or Vulkan. WebGL2 [does not allow inefficient legacy features](https://en.wikipedia.org/wiki/OpenGL)  such as immediate mode, direct-mode rendering, or geometry shaders. This repository is designed to be a playground to see if NiiVue can be enhanced by adopting [WebGPU](https://en.wikipedia.org/wiki/WebGPU). Our goal is to start with the [elegant minimal demo from Will Usher](https://github.com/Twinklebear/webgpu-volume-raycaster) and slowly explore porting NiiVue shaders and functions to WebGPU.
+
+Several considerations to bear in mind:
+
+ - WebGPU is unlikely to ever support OpenGL-based devices. One of the aims for NiiVue is to empower users who do not have access to the latest hardware.
+ - The WebGPU specification has not been finalized, and it remains a moving target.
+ - It took over five **years** after the stable specification of WebGL2 was finalized for it to be supported by all major browsers.
+ - For visualization, the primary benefit of WebGPU over WebGL2 is the efficiency of a command buffer instead of many separate draw calls. However, NiiVue uses only a handful of draw calls, with the fragment shader being the main bottleneck. Therefore, WebGL and WebGPU are likely to provide similar visualization performance.
+ - WebGPU promises to revolutionize the use of graphics hardware for compute tasks. While NiiVue already uses WebGL2 for some compute tasks, there is scope to enhance these functions. The primary interest in WebGPU for NiiVue is for compute, not visualization.
+
+## Building
+
+```bash
+git clone https://github.com/niivue/niivue-webgpu
+cd niivue-webgpu
+npm install
+npm run serve
+```
+
+** Warning: the WebGPU language has been changed: you must run Chrome 113 or later **
+
+** As of Mid-April 2023 you may need to run [Chrome Canary](https://www.google.com/chrome/canary/) instead of the current Chrome release **
+
+A local live demo will be hosted on `localhost:8080`.
---
+++ src/colortables.js
@@ -0,0 +1,134 @@
+import * as cmaps from "./cmaps";
+
+export const colortables = function () {
+  this.version = 0.1;
+  this.gamma = 1.0;
+  let cmapsSorted = [];
+  console.log(cmaps)
+  if (cmaps.length > 0) return this.cmaps;
+  for (const [key] of Object.entries(cmaps)) {
+    if (key.startsWith("$")) continue; //ignore drawing maps
+    cmapsSorted.push(key);
+  }
+  cmapsSorted.sort();
+  this.cluts = {};
+  for (let i = 0; i < cmapsSorted.length; i++) {
+    let key = cmapsSorted[i];
+    this.cluts[key] = cmaps[key];
+  }
+};
+
+colortables.prototype.addColormap = function (key, cmap) {
+  this.cluts[key] = cmap;
+};
+
+colortables.prototype.colorMaps = function () {
+  return Object.keys(this.cluts);
+};
+
+// returns key name if it exists, otherwise returns default "gray"
+/*colortables.prototype.key2key = function (key = "") {
+  let cmap = this.cluts[key];
+  if (cmap !== undefined) return key;
+  console.log("No color map named " + key);
+  return "Gray";
+};*/
+
+colortables.prototype.colormapFromKey = function (name) {
+  let cmap = this.cluts[name];
+  if (cmap !== undefined) return cmap;
+  console.log("No color map named " + name);
+  cmap = {
+    min: 0,
+    max: 0,
+    R: [0, 255],
+    G: [0, 255],
+    B: [0, 255],
+    A: [0, 255],
+    I: [0, 255],
+  };
+  return cmap;
+};
+
+// not included in public docs
+colortables.prototype.colormap = function (key = "") {
+  let cmap = this.colormapFromKey(key);
+  return this.makeLut(cmap.R, cmap.G, cmap.B, cmap.A, cmap.I);
+}; // colormap()
+
+// not included in public docs
+colortables.prototype.makeDrawLut = function (name) {
+  let cmap = [];
+  if (typeof name === "object") cmap = name;
+  else cmap = cmaps[name];
+  if (cmap === undefined) {
+    cmap = {
+      min: 0,
+      max: 0,
+      R: [0, 255, 0, 0, 255, 0, 255],
+      G: [0, 0, 255, 0, 255, 255, 0],
+      B: [0, 0, 0, 255, 0, 255, 255],
+      A: [0, 255, 255, 255, 255, 255, 255],
+    };
+  }
+  if (cmap.R.length < 256) {
+    let j = 256 - cmap.R.length;
+    for (let i = 0; i < j; i++) {
+      //make all unused slots opaque red
+      cmap.R.push(255);
+      cmap.G.push(0);
+      cmap.B.push(0);
+      cmap.A.push(255);
+    }
+  }
+  if (!cmap.hasOwnProperty("labels")) cmap.labels = [];
+  if (cmap.labels.length < 256) {
+    let j = cmap.labels.length;
+    for (let i = j; i < 256; i++) {
+      //make all unused slots opaque red
+      cmap.labels.push(i.toString());
+    }
+  }
+  var lut = new Uint8ClampedArray(256 * 4);
+  var k = 0;
+  for (let i = 0; i < 256; i++) {
+    lut[k++] = cmap.R[i]; //Red
+    lut[k++] = cmap.G[i]; //Green
+    lut[k++] = cmap.B[i]; //Blue
+    lut[k++] = cmap.A[i]; //Alpha
+  }
+  return {
+    lut: lut,
+    labels: cmap.labels,
+  };
+};
+
+// not included in public docs
+colortables.prototype.makeLut = function (Rs, Gs, Bs, As, Is) {
+  //create color lookup table provided arrays of reds, greens, blues, alphas and intensity indices
+  //intensity indices should be in increasing order with the first value 0 and the last 255.
+  // this.makeLut([0, 255], [0, 0], [0,0], [0,128],[0,255]); //red gradient
+  var lut = new Uint8ClampedArray(256 * 4);
+  for (var i = 0; i < Is.length - 1; i++) {
+    //return a + f * (b - a);
+    var idxLo = Is[i];
+    var idxHi = Is[i + 1];
+    var idxRng = idxHi - idxLo;
+    var k = idxLo * 4;
+    for (var j = idxLo; j <= idxHi; j++) {
+      var f = (j - idxLo) / idxRng;
+      lut[k++] = Rs[i] + f * (Rs[i + 1] - Rs[i]); //Red
+      lut[k++] = Gs[i] + f * (Gs[i + 1] - Gs[i]); //Green
+      lut[k++] = Bs[i] + f * (Bs[i + 1] - Bs[i]); //Blue
+      lut[k++] = As[i] + f * (As[i + 1] - As[i]); //Alpha
+    }
+  }
+  if (this.gamma === 1.0) return lut;
+  for (var i = 0; i < 255 * 4; i++) {
+    if (i % 4 === 3) continue; //gamma changes RGB, not Alpha
+    lut[i] = Math.pow(lut[i] / 255, 1 / this.gamma) * 255;
+  }
+  return lut;
+}; // makeLut()
+
+export const cmapper = new colortables();
